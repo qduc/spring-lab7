3fb6677396d5bb3b953088a790e05bc581b1e042
[Lampart] Nguyen Quang Duc <quang_duc@lampart-vn.com>
2019-10-31 11:50:17 +0700

    Add thymeleaf-layout-dialect

--- application/src/main/java/com/lampart/duc/lab7/application/Lab7Application.java
+++ application/src/main/java/com/lampart/duc/lab7/application/Lab7Application.java
@@ -1,12 +1,24 @@
 package com.lampart.duc.lab7.application;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = "com.lampart.duc.lab7")
 public class Lab7Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Lab7Application.class, args);
+    }
+
+    /**
+     * Configure thymeleaf-layout-dialect
+     *
+     * @link https://ultraq.github.io/thymeleaf-layout-dialect/
+     */
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
     }
 }
