16edd7e03674f61599d15754b5da52c899ec9416
[Lampart] Nguyen Quang Duc <quang_duc@lampart-vn.com>
2019-11-08 12:02:44 +0700

    Add edit user feature

--- application/src/main/java/com/lampart/duc/lab7/application/controller/UserController.java
+++ application/src/main/java/com/lampart/duc/lab7/application/controller/UserController.java
@@ -4,10 +4,9 @@ import com.lampart.duc.lab7.api.data.DataService;
 import com.lampart.duc.lab7.service.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(UserController.urlPrefix)
@@ -36,6 +35,30 @@ public class UserController {
 
     @PostMapping("add")
     public String addPost(@ModelAttribute UserDto userDto) {
+        userDataService.save(userDto);
+        return "redirect:" + urlPrefix;
+    }
+
+    @GetMapping("edit/{id}")
+    public String editGet(
+            @PathVariable(value="id") Long id,
+            Model model
+    ) {
+        Optional<UserDto> user = userDataService.findById(id);
+        if (!user.isPresent()) {
+            return "error/404";
+        }
+
+        model.addAttribute("user", user.get());
+        return viewRoot + "add_edit";
+    }
+
+    @PostMapping("edit/{id}")
+    public String editPost(
+            @PathVariable(value="id") Long id,
+            @ModelAttribute UserDto userDto
+    ) {
+        userDto.setId(id);
         userDataService.save(userDto);
         return "redirect:" + urlPrefix;
     }
